bf9a452a184a3414ebf88fdb0090ee66c7a80f81
ArminPosadas <armin.posadas@uniat.edu.mx>
2026-03-17 13:04:13 -0600

    BugFixes to windows

--- Scripts/ButtonPopUp.gd
+++ Scripts/ButtonPopUp.gd
@@ -10,3 +10,13 @@ func _on_pressed() -> void:
 func _on_cv_close_requested() -> void:
 	if popup_window:
 		popup_window.hide()
+
+
+func _on_about_me_close_requested() -> void:
+	if popup_window:
+		popup_window.hide()
+
+
+func _on_contacts_close_requested() -> void:
+	if popup_window:
+		popup_window.hide()
