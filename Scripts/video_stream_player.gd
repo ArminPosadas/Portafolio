e511e3bc711b8a3ce29810751141f45b4af69446
ArminPosadas <armin.posadas@uniat.edu.mx>
2026-05-28 13:39:33 -0600

    Several changes or smt

---
+++ Scripts/video_stream_player.gd
@@ -0,0 +1,12 @@
+extends VideoStreamPlayer
+
+@onready var btn = $"../../Pause"
+var icon_play = "res://Assets/Sprites/Logo.png"
+var icon_pause = "res://Assets/Sprites/Sprite-0001.png"
+
+# func _on_pause_button_down() -> void:
+	# paused = !paused
+	# if paused:
+		# btn.icon = icon_play
+	# else:
+		# btn.icon = icon_pause
